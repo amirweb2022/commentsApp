61d06a1bc6db15bd80225a088c2c1f41cc6b6477
amir <amir.hz.09921103757@gmail.com>
2022-09-27 20:35:17 +0330

    add header

--- src/services/postCommentService.jsx
+++ src/services/postCommentService.jsx
@@ -1,5 +1,11 @@
 import http from "./httpServices";
 
 export function postComment(data) {
-  return http.post("/comments", data);
+  const token = "97e0d315477f435489cf04904c9d0e6co";
+  const header = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return http.post("/comments", data, header);
 }
